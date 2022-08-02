2a78f8f38aebd620a5b09661aa202de9acfbd48e
beenchangseo <98647896+beenchangseo@users.noreply.github.com>
2022-08-02 12:04:24 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < n-i; ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i-1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
