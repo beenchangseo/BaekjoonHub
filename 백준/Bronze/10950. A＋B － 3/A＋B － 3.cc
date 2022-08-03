00c1656f24d32a18f42da5842c03800cfcfab849
beenchangseo <98647896+beenchangseo@users.noreply.github.com>
2022-08-03 17:45:35 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, T;
+
+int main(){
+    cin >> T;
+    for(int i=0; i<T; i++){
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    return 0;
+}
