ad3e1707ac66efae9abf60facea35e3300bef17d
beenchangseo <98647896+beenchangseo@users.noreply.github.com>
2022-08-03 17:42:36 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int sum(int n);
+int main() {
+    int n, result;
+    cin >> n;
+    result = sum(n);
+    cout << result;
+    return 0;
+}
+int sum(int n){
+    if (n == 0) return 0;
+    return n + sum(n-1);
+}
+
